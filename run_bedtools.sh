44993e2130f236d4d700742b8ff4cc522a2b0c55
Zachary Lounsberry <zlounsberry@gmail.com>
2019-10-10 14:09:34 -0700

    Tested and currently working

--- run_bedtools.sh
+++ run_bedtools.sh
@@ -23,6 +23,7 @@ do
 	alt=$6
 
 # First make bed file:
+start=$(echo "${start} - 1" | bc)
 start_ref=$(echo "${start} - 200" | bc)
 end_ref=$(echo "${end} + 200" | bc)
 
@@ -33,16 +34,16 @@ bedtools getfasta -fi ${reference} -bed ${marker}.bed -fo ${marker}.fa
 
 paste --delimiter="" <(echo "${marker}	") <(sed -n '2p' ${marker}.fa) <(echo "[${ref}/${alt}]") <(sed -n '4p' ${marker}.fa)
 
-#rm ${marker}.bed ${marker}.fa
+rm ${marker}.bed ${marker}.fa
 
 x=$(( $x + 1 ))
 done > ${output}
 
-for marker in $(cut -f1 ${output}); do
+for marker in $(awk '{print $1}' ${output}); do
 	echo ">${marker}"
 	awk -v var=${marker} '$1==var {print $2}' ${output} | sed 's/\[//g' | sed 's/\/.*]//g' | sed 's/-//g'
 done > ${output}.check.fasta
 
 blat ${reference} ${output}.check.fasta -stepSize=5 -repMatch=2253 -minScore=0 -minIdentity=0 -out=blast8 ${output}.check.fasta.blat.out
 
-awk '($3==100 && $4==401) {print}' ${output}.check.fasta.blat.out > correct.matches.final.out 
+awk '($3==100 && $4>399) {print}' ${output}.check.fasta.blat.out > correct.matches.final.out
